e389fc6af4bd725ce69ad4474b7820b6e66c4eb4
Cédric Levasseur <cedric.levasseur@ipocus.net>
2012-11-24 15:39:04 +0100

    Fix Dropdown Menu for Mobile Device

--- src/classes/Scripts.php
+++ src/classes/Scripts.php
@@ -19,19 +19,7 @@ class Scripts
 		<script src='bootstrap/js/bootstrap.min.js'></script>
 		<script src='bootstrap/js/bootstrap-modalmanager.js'></script>
 		<script src='bootstrap/js/bootstrap-modal.js'></script>
-		<script>
-			!function ($) {
-				$(function(){
-				  // Fix for dropdowns on mobile devices
-				  $('body').on('touchstart.dropdown', '.dropdown-menu', function (e) { 
-				      e.stopPropagation(); 
-				  });
-				  $(document).on('click','.dropdown-menu a',function(){
-				      document.location = $(this).attr('href');
-				  });
-				})
-			}(window.jQuery)
-		</script>
+
 		
 		<!-- PLUpload -->
 		<script src='plupload/js/plupload.full.js'></script>
@@ -50,8 +38,20 @@ class Scripts
 		<script src='src/js/keyboard.js'></script>
 
 		<script src='inc/jquery.scrollstop.js'></script>
-		<script src='inc/jquery.lazyload.min.js'></script>		
-		";
+		<script src='inc/jquery.lazyload.min.js'></script>				
+		<script>
+		      !function ($) {
+			$(function(){
+			  // Fix for dropdowns on mobile devices
+			  $('body').on('touchstart.dropdown', function (e) { 
+			      e.stopPropagation(); 
+			  });
+			  $(document).on('click','.dropdown-menu a',function(){
+			      document.location = $(this).attr('href');
+			  });
+			})
+		      }(window.jQuery)
+		</script>";
 		if(CurrentUser::$admin || CurrentUser::$uploader){
 			echo "<script src='src/js/admin.js'></script>";
 			echo "<script src='src/js/plupload-ui.js'></script>";
